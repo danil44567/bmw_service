103e85dd9a5ec5800676e008a878e1272c1bb48d
Danil <gladyshev-g@bk.ru>
2024-05-22 23:53:46 +0500

    Правки для нормального деплоя

--- bmw_service/settings.py
+++ bmw_service/settings.py
@@ -34,8 +34,7 @@ SECRET_KEY = 'django-insecure-!3nlj$ddw604pprab_=mpxotw@&z+m%u3#@gnq&&(^u6c7qh0$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 # Application definition
 
@@ -130,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
