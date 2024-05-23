b73556da2c657bca05a9b859de6a27621d7a5438
Danil <gladyshev-g@bk.ru>
2024-05-23 23:03:23 +0500

    Ещё вынес в env

--- bmw_service/settings.py
+++ bmw_service/settings.py
@@ -29,10 +29,10 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!3nlj$ddw604pprab_=mpxotw@&z+m%u3#@gnq&&(^u6c7qh0$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
